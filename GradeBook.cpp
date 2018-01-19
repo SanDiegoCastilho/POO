013df451f145fb91202660b8588a58e27c8ae952
SanDiego <sandiegocastilho@gmail.com>
2018-01-19 17:04:07 -0300

    Passando parametros

--- GradeBook.cpp
+++ GradeBook.cpp
@@ -1,25 +1,34 @@
-//San Diego 17/01/2018 - 16:03
+//San Diego  - modificado - 19/01/2018 - 16:57
 //Introdução a Orientação a Objetos com C++.
 //Criando uma classe GradeBook e imprimir uma mensagem na tela.
 #include <iostream>
+#include <string>
 
+using std::string;
+using std::getline;
 using std::cout;
 using std::cin;
+using std::endl;
 
 //Declarando a classe GradeBook.
 class GradeBook{
 public:
 
 	//Função que imprime uma mensagem na tela.
-	void displayMessage(){
-		cout << "Welcome To The Grade Book!";
+	void displayMessage(string courseName){
+		cout << "Welcome To The Grade Book!\n" << courseName << endl;
 	}
 };
 
 int main(){
+	string nameOfCourse;
 	GradeBook myGradeBook;
 
-	myGradeBook.displayMessage();
+	cout << "Digite o nome do curso: ";
+	getline(cin, nameOfCourse);
+	cout << endl;
+
+	myGradeBook.displayMessage(nameOfCourse);
 
 	return 0;
 }
