7bb39608af36d99005b3d2c3c56aac4a54958494
SanDiego <sandiegocastilho@gmail.com>
2018-01-19 17:23:36 -0300

    membros de dados, funcoes get, funcoes set

--- GradeBook.cpp
+++ GradeBook.cpp
@@ -14,21 +14,38 @@ using std::endl;
 class GradeBook{
 public:
 
-	//Função que imprime uma mensagem na tela.
-	void displayMessage(string courseName){
-		cout << "Welcome To The Grade Book!\n" << courseName << endl;
+	//Função responsável por guardar o nome do curso.
+	void setCourseName(string name){
+		courseName = name; //Armazena o nome do curso no objeto.
 	}
+
+	//Função responsável por obter o nome do curso.
+	string getCourseName (){
+		return courseName; //Retorna o courseName do objeto.
+	}
+
+	//Função que imprime uma mensagem na tela.
+	void displayMessage(){
+		cout << "Welcome To The Grade Book!\n" << getCourseName() << endl;
+	}
+
+private:
+	string courseName;
 };
 
 int main(){
 	string nameOfCourse;
 	GradeBook myGradeBook;
 
-	cout << "Digite o nome do curso: ";
+	cout << "O nome inicial do Curso é: " << myGradeBook.getCourseName() << endl;
+
+	cout << "\nDigite o novo nome do curso: ";
 	getline(cin, nameOfCourse);
 	cout << endl;
 
-	myGradeBook.displayMessage(nameOfCourse);
+	myGradeBook.setCourseName(nameOfCourse);
+
+	myGradeBook.displayMessage();
 
 	return 0;
 }
